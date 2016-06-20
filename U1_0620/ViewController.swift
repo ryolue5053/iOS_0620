441d54228d3a5f849b3fc94fefe26387b8d9d11d
許律律 <ryolue@hsudemacbook-pro.local>
2016-06-20 20:35:46 +0800

    Initial Commit

---
+++ U1_0620/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  U1_0620
+//
+//  Created by Hsu Lu wei on 2016/6/20.
+//  Copyright © 2016年 Hsu Lu wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
